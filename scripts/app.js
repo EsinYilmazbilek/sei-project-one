b5a52826a1f7a67c3a5e42550517e5761410f9cd
Esin Yilmazbilek <esinyilmazbilek@gmail.com>
2021-11-15 14:26:26 +0000

    third push aliens are moving

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,5 @@
 /* eslint-disable indent */
+
 //* Grid Variables *//
 
 const grid = document.querySelector('.grid')
@@ -9,7 +10,7 @@ const gridCellCount =  width * width
 
 //* Borg Variables *//
 
-const borgs = [
+let borgs = [
   13,14,15,16,17,18,19,
   25,26,27,28,29,30,31,
   37,38,39,40,41,42,43,
@@ -18,7 +19,7 @@ const borgs = [
 
 //* Starship Variables *//
 
-const starShipIndex = 120
+let starShipIndex = 120
 
 
 //* Building the Grids *//
@@ -48,5 +49,121 @@ createBorg()
 
 //* Building the StarShip *//
 
-
 cells[starShipIndex].classList.add('starship')
+
+
+
+
+
+//* Moving the Borg - Starting the Game upon clicking the start button*//
+
+const startButton = document.querySelector('.start')
+
+function removeBorg(){
+  borgs.forEach((index) => {
+    cells[index].classList.remove('borgs')
+  })
+}
+
+function addBorg(){
+  borgs.forEach((index) => {
+    cells[index].classList.add('borgs')
+  })
+}
+
+function handleGameStart() {
+  window.setInterval(() => {
+    removeBorg()
+    borgs = borgs.map((index) => {  
+      return index += 1
+    })
+    addBorg()
+      // borgs.forEach((index) => {  
+  //   cells[index].classList.remove('borgs'),
+  //   index ++
+  //   cells[index].classList.add('borgs')
+  //   return
+  // })
+  }, 500)
+}
+
+startButton.addEventListener('click', handleGameStart)
+
+
+
+// startButton.onclick = function moveBorgs() {
+// }
+
+
+
+//* Move the StarShip *//
+
+function removeStarship(){
+    cells[starShipIndex].classList.remove('starship')
+}
+
+function addStarship(){
+    cells[starShipIndex].classList.add('starship')
+}
+
+function moveStarShip(e) {
+  // console.log(e.code)
+  const x = starShipIndex % width
+
+  removeStarship()
+  switch (e.code) {
+    case 'ArrowRight':
+      if (x < width - 1) {
+        starShipIndex++
+      }
+      break
+    case 'ArrowLeft':
+      if (x > 0) {
+        starShipIndex--
+      }
+      break
+    default:
+      console.log('no movement')
+  }
+  addStarship()
+}
+
+document.addEventListener('keyup', moveStarShip)
+
+
+
+
+//* Startship Laser *//
+
+// const starshipLaserIndex = document.querySelector('.starshipLaser')
+
+
+let starshipLaserIndex = starShipIndex
+
+cells[starshipLaserIndex].classList.add('starshipLaser')
+
+function removeStarshipLaser(){
+  cells[starshipLaserIndex].classList.remove('starshipLaser')
+}
+
+function addStarshipLaser(){
+  cells[starshipLaserIndex].classList.add('starshipLaser')
+}
+
+
+function handleStarshipLaser(e) {
+console.log(e)
+// const y = starshipLaserIndex / width
+
+if (e.key === 'x') {
+  console.log('hello')
+  removeStarshipLaser()
+  starshipLaserIndex -= 12
+  addStarshipLaser()
+  return
+} 
+
+}
+
+document.addEventListener('keydown', handleStarshipLaser)
+
