e8f9a0c9b8d6a3b931b374fd8fa9c67d4687309c
Esin Yilmazbilek <esinyilmazbilek@gmail.com>
2021-11-16 14:42:49 +0000

    fourth push with array working

--- scripts/app.js
+++ scripts/app.js
@@ -8,13 +8,38 @@ const width = 12
 const gridCellCount =  width * width
 
 
+
 //* Borg Variables *//
 
 let borgs = [
-  13,14,15,16,17,18,19,
-  25,26,27,28,29,30,31,
-  37,38,39,40,41,42,43,
-  49,50,51,52,53,54,55
+  { index: 13, isAlive: true },
+  { index: 14, isAlive: true }, 
+  { index: 15, isAlive: true }, 
+  { index: 16, isAlive: true }, 
+  { index: 17, isAlive: true }, 
+  { index: 18, isAlive: true }, 
+  { index: 19, isAlive: true }, 
+  { index: 25, isAlive: true }, 
+  { index: 26, isAlive: true }, 
+  { index: 27, isAlive: true }, 
+  { index: 28, isAlive: true }, 
+  { index: 29, isAlive: true },
+  { index: 30, isAlive: true }, 
+  { index: 31, isAlive: true }, 
+  { index: 37, isAlive: true }, 
+  { index: 38, isAlive: true }, 
+  { index: 39, isAlive: true }, 
+  { index: 40, isAlive: true }, 
+  { index: 41, isAlive: true }, 
+  { index: 42, isAlive: true }, 
+  { index: 43, isAlive: true }, 
+  { index: 49, isAlive: true }, 
+  { index: 50, isAlive: true }, 
+  { index: 51, isAlive: true }, 
+  { index: 52, isAlive: true }, 
+  { index: 53, isAlive: true }, 
+  { index: 54, isAlive: true }, 
+  { index: 55, isAlive: true } 
 ]
 
 //* Starship Variables *//
@@ -34,16 +59,24 @@ function createGrid() {
 }
 createGrid()
 
+const border = [0,12,24,36,48,60,72,84,96,108,120,132,
+11,23,35,47,59,71,83,95,107,119,131,143]
+
+function createBorder(){
+  border.forEach((index) => {
+    cells[index].classList.add('border')
+    return
+  })
+}
+createBorder()
 
 //* Building the Borg *//
 
 function createBorg(){
-  borgs.forEach((index) => {
-    cells[index].classList.add('borgs')
-    return
+  borgs.forEach((borg) => {
+    cells[borg.index].classList.add('borgs')
   })
 }
-
 createBorg()
 
 
@@ -52,51 +85,61 @@ createBorg()
 cells[starShipIndex].classList.add('starship')
 
 
-
-
-
-//* Moving the Borg - Starting the Game upon clicking the start button*//
+//* Moving the Borg - game starts upon clicking the start button *//
 
 const startButton = document.querySelector('.start')
 
 function removeBorg(){
-  borgs.forEach((index) => {
-    cells[index].classList.remove('borgs')
+  borgs.forEach((borg) => {
+    cells[borg.index].classList.remove('borgs')
   })
 }
 
 function addBorg(){
-  borgs.forEach((index) => {
-    cells[index].classList.add('borgs')
+  borgs.forEach((borg) => {
+    if (borg.isAlive) {
+      cells[borg.index].classList.add('borgs')
+    }
   })
 }
 
+let direction = 1
+
 function handleGameStart() {
-  window.setInterval(() => {
-    removeBorg()
-    borgs = borgs.map((index) => {  
-      return index += 1
-    })
+  window.setInterval(() => {    
+    const rightBorder = borgs[borgs.length - 1].index % width === width - 1
+    const leftBorder = borgs[0].index % width === 0
+
+    removeBorg() 
+
+    if (rightBorder && direction === 1) {
+      borgs = borgs.map(borg => {
+        borg.index += width 
+        return borg
+      })
+      direction = -1
+    } else if (leftBorder && direction === -1) {
+      borgs = borgs.map(borg => {
+        borg.index += width
+        return borg
+      })
+      direction = 1
+    } else {
+      borgs = borgs.map(borg => {
+        borg.index += direction
+        return borg
+      })
+    }
+
     addBorg()
-      // borgs.forEach((index) => {  
-  //   cells[index].classList.remove('borgs'),
-  //   index ++
-  //   cells[index].classList.add('borgs')
-  //   return
-  // })
-  }, 500)
+  }, 860)
 }
 
 startButton.addEventListener('click', handleGameStart)
 
 
 
-// startButton.onclick = function moveBorgs() {
-// }
-
-
-
-//* Move the StarShip *//
+//* Moving the StarShip *//
 
 function removeStarship(){
     cells[starShipIndex].classList.remove('starship')
@@ -107,7 +150,7 @@ function addStarship(){
 }
 
 function moveStarShip(e) {
-  // console.log(e.code)
+
   const x = starShipIndex % width
 
   removeStarship()
@@ -132,15 +175,13 @@ document.addEventListener('keyup', moveStarShip)
 
 
 
-
 //* Startship Laser *//
 
-// const starshipLaserIndex = document.querySelector('.starshipLaser')
+const starshipLaserIndex = document.querySelector('.starshipLaser')
 
-
-let starshipLaserIndex = starShipIndex
-
-cells[starshipLaserIndex].classList.add('starshipLaser')
+let starshipLaserIndex = starShipIndex - width
+let timerId = null
+let starshipFired = false
 
 function removeStarshipLaser(){
   cells[starshipLaserIndex].classList.remove('starshipLaser')
@@ -150,20 +191,67 @@ function addStarshipLaser(){
   cells[starshipLaserIndex].classList.add('starshipLaser')
 }
 
+function handleStarshipLaser() {
 
-function handleStarshipLaser(e) {
-console.log(e)
-// const y = starshipLaserIndex / width
+// const y = Math.floor(starshipLaserIndex % width)
+starshipLaserIndex = starShipIndex - width
 
-if (e.key === 'x') {
-  console.log('hello')
+timerId = window.setInterval((e) => {
+
+  if (starshipLaserIndex <= width) {
+  e.code === 'KeyX'
   removeStarshipLaser()
-  starshipLaserIndex -= 12
-  addStarshipLaser()
+  clearInterval(timerId)
+  starshipLaserIndex = starShipIndex - width
   return
-} 
+} else if (cells[starshipLaserIndex].classList.contains('borg')) {
+  cells[starshipLaserIndex].classList.remove('starshipLaser')
+  cells[starshipLaserIndex].classList.remove('starshipLaser')
 
+  clearInterval(timerId)
+  starshipFired = false
+  return
+}
+removeStarshipLaser()
+starshipLaserIndex -= width
+addStarshipLaser
+return
+}, 200)
 }
 
 document.addEventListener('keydown', handleStarshipLaser)
 
+
+
+// const laserAudioStarship = document.createElement('audioStarship')
+// laserAudioStarship.src = './/development/projects/project-1/assets'
+// laserAudioStarship.play()
+// laserAudioStarship.pause()
+
+
+
+//* Borgs Laser *//
+
+const laserAudioBorg = document.createElement('audioBorg')
+laserAudioBorg.src = './/development/projects/project-1/assets'
+laserAudioBorg.play()
+laserAudioBorg.pause()
+
+
+//* Game Ends *//
+
+function borgsWin() {
+
+  if (cells[borgs].classList.contains('borgs' || 'starShipIndex')) {
+  document.querySelector('.game-ends-borgs-win').innerHTML = 'You will be assimilated'
+  }
+}
+borgsWin()
+
+
+function starshipWins() {
+  if (cells[borgs].classList.contains('starshipIndex')) {
+  document.querySelector('.game-ends').innerHTML = 'You have defeated the Borg. Take us out of orbit' 
+  }
+}
+starshipWins()
